c70202962957a24cd7c0dc9b956b146e37d0ffa5
Frizlab <fload@me.com>
2022-03-06 15:33:19 +0100

    Drop cocoapods, add SPM

--- Package.swift
+++ Package.swift
@@ -1,15 +1,9 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.5
 import PackageDescription
 
+
 let package = Package(
-    name: "AlignedCollectionViewFlowLayout",
-    products: [
-        .library(name: "AlignedCollectionViewFlowLayout", targets: ["AlignedCollectionViewFlowLayout"])
-    ],
-    targets: [
-        .target(
-            name: "AlignedCollectionViewFlowLayout",
-            path: "AlignedCollectionViewFlowLayout"
-        )
-    ]
+	name: "AlignedCollectionViewFlowLayout",
+	products: [.library(name: "AlignedCollectionViewFlowLayout", targets: ["AlignedCollectionViewFlowLayout"])],
+	targets: [.target(name: "AlignedCollectionViewFlowLayout")]
 )
